7131579074eba64d90df967a79f9f47782d89c77
KotatuBot <kotatubot@gmail.com>
2019-11-24 21:39:44 +0900

    lightning-front

---
+++ lightning-front/src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div id="app">
+    <SourceView/>
+  </div>
+</template>
+
+<script>
+import SourceView from './components/SourceView.vue'
+
+export default {
+  name: 'app',
+  components: {
+    SourceView
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ lightning-front/src/components/SourceView.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="sourceview">
+                        <h3> SoureTEST</h3>
+			<tr v-for="code in codeview" v-bind:key="code.id">
+				<td>{{code.id}}</td>
+				<td class="codeline">{{ code.content }}</td>
+			</tr>
+		<hr/>
+		<button v-on:click="data_send">Send</button>
+
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SourceView',
+  data: function(){
+          return{
+		codeview: [],
+          }
+  },
+
+ methods: {
+
+	data_send: function(event){
+	if(event){
+
+		var code_data = "<?php\n\ngila::controller('blog', 'blog/controllers/blog', 'Blog');\n";
+		var code_list = code_data.split('\n');
+
+		for (var number=0;number<code_list.length;number++){
+
+			var tmp = {id:number,content:code_list[number]}
+			this.codeview.push(tmp)
+		}
+
+	}
+
+	}
+	
+
+ },
+
+}
+</script>
+<style>
+.codeline{
+
+	text-align: left;
+	padding-left: 5%;
+	width: 1500px;
+
+}
+</style>
