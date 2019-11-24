9db76f9e3831e6a26a82363d499b57a0955757ab
KotatuBot <kotatubot@gmail.com>
2019-11-24 22:35:01 +0900

    click

--- lightning-front/src/components/SourceView.vue
+++ lightning-front/src/components/SourceView.vue
@@ -2,11 +2,20 @@
   <div class="sourceview">
                         <h3> SoureTEST</h3>
 			<tr v-for="code in codeview" v-bind:key="code.id">
-				<td>{{code.id}}</td>
+				<td v-on:click="onwrite(code.id)">{{code.id}}</td>
 				<td class="codeline">{{ code.content }}</td>
 			</tr>
 		<hr/>
 		<button v-on:click="data_send">Send</button>
+
+
+                <div v-if="flag" class="codeline">
+			<form action="#" method="post">
+				<textarea name="comment" cols="30" rows="5"></textarea>	
+				<p>{{currentnumber}}</p>
+			</form>
+                
+                </div>
 
 
   </div>
@@ -18,6 +27,8 @@ export default {
   data: function(){
           return{
 		codeview: [],
+                currentnumber: 0,
+                flag: false,
           }
   },
 
@@ -31,13 +42,25 @@ export default {
 
 		for (var number=0;number<code_list.length;number++){
 
-			var tmp = {id:number,content:code_list[number]}
+			var tmp = {id:number,content:code_list[number],flag:false,message:''}
 			this.codeview.push(tmp)
 		}
 
 	}
 
-	}
+	},
+
+        onwrite: function(code){
+                for (var number=0;number<this.codeview.length;number++){
+
+                        if (code===this.codeview[number].id){
+
+                               this.flag = true;
+				this.currentnumber = code;
+                                
+                             }
+		}
+        }
 	
 
  },
@@ -49,7 +72,9 @@ export default {
 
 	text-align: left;
 	padding-left: 5%;
-	width: 1500px;
+        padding-bottom: -100px;
+	width: 500px;
+        height: 3%;
 
 }
 </style>
