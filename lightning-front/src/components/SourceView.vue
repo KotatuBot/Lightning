2bc5198819833f2730ca5aaab2aaa2b3a6c5546f
KotatuBot <kotatubot@gmail.com>
2019-11-25 22:20:56 +0900

    Add memo

--- lightning-front/src/components/SourceView.vue
+++ lightning-front/src/components/SourceView.vue
@@ -4,17 +4,36 @@
 			<tr v-for="code in codeview" v-bind:key="code.id">
 				<td v-on:click="onwrite(code.id)">{{code.id}}</td>
 				<td class="codeline">{{ code.content }}</td>
+                                <div v-if="code.mflag">
+                                        <button v-on:click="display_memo(code.id)">+</button>
+                                </div>
 			</tr>
 		<hr/>
 		<button v-on:click="data_send">Send</button>
 
 
                 <div v-if="flag" class="codeline">
-			<form action="#" method="post">
-				<textarea name="comment" cols="30" rows="5"></textarea>	
-				<p>{{currentnumber}}</p>
-			</form>
+			<div>
+				<textarea name="comment" cols="30" rows="5" v-model="message"></textarea>	
+                                <button v-on:click="adding_memo(currentnumber)" class="btn btn-secondary">Send</button>
+			</div>
                 
+                </div>
+
+                <div>
+                        <div v-for="code in codeview" v-bind:key="code.id">
+
+                                <div v-if="code.dflag">
+                                        <div class="codeline">
+                                                <h3>{{code.id}} line</h3>
+                                                <hr>
+                                                <p>{{code.message}}</p>
+                                        </div>
+
+                                </div>
+
+
+                        </div>
                 </div>
 
 
@@ -28,7 +47,10 @@ export default {
           return{
 		codeview: [],
                 currentnumber: 0,
+                message: '',
+                writeing_memo: [],
                 flag: false,
+                
           }
   },
 
@@ -42,7 +64,7 @@ export default {
 
 		for (var number=0;number<code_list.length;number++){
 
-			var tmp = {id:number,content:code_list[number],flag:false,message:''}
+			var tmp = {id:number,content:code_list[number],flag:false,message:'',mflag:false,dflag:false}
 			this.codeview.push(tmp)
 		}
 
@@ -60,6 +82,41 @@ export default {
                                 
                              }
 		}
+        },
+
+        adding_memo: function(index){
+
+                for(var j=0;j<this.codeview.length;j++){
+                        if(j===index){
+                               this.codeview[j].mflag = true;
+                               this.codeview[j].message = this.message;
+                        }       
+                        
+                 }
+
+                var tmp = {}
+                tmp['id']=index
+                tmp['memo']=this.message;
+                this.writeing_memo.push(tmp)
+                this.message = "";
+                this.flag = false;
+                
+        },
+
+        display_memo: function(index){
+                for(var s=0;s<this.codeview.length;s++){
+                        if(s===index){
+
+                               if(this.codeview[s].dflag==false){
+                                        this.codeview[s].dflag = true;
+                               }else{
+                                       
+                                        this.codeview[s].dflag = false;        
+                               }
+                        }       
+                        
+                 }
+                
         }
 	
 
